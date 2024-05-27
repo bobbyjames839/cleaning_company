4bb09bd2a85e82b5e88d8e3e77c3d3ba94f008cf
Bobby Davidson <bobbyjames839@gmail.com>
2024-05-27 16:10:15 +0100

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import '../styles/Header.css'
+import logo from '../images/logo.png'
+
+export const Header = () => {
+  return (
+    <div className="header">
+      
+      <img src={logo}/>
+      
+      <div className="header-right">
+        <a href="#">Home</a>
+        <a href="#">About Us</a>
+        <a href="#">The App</a>
+        <a href="#">FAQs</a>
+        <a href="#">Footer</a>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import '../styles/Home.css'
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <span className="shape1"></span>
+    </div>
+  )
+}
